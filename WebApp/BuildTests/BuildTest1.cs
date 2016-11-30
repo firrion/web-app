4547a577bd081ec79fc7b12a19efb31ad13ea3a6
Alexey <sora@list.ru>
2016-11-30 10:23:45 +0600

    test

---
+++ WebApp/BuildTests/BuildTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp.Controllers;
+using System.Web.Mvc;
+
+namespace BuildTests
+{
+    [TestClass]
+    public class BuildTest1
+    {
+        HomeController homeController = new HomeController();
+        [TestMethod]
+        public void Test1()
+        {
+            var result = (ViewResult)homeController.Index();
+            Assert.AreEqual("Index", result.ViewName);
+        }
+    }
+}
